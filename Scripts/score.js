5aca0d631ec18574901404cb9b041fc72150023b
Eduardo <tkd13000@gmail.com>
2024-08-05 09:31:31 -0600

    It can count points or score

--- Scripts/score.js
+++ Scripts/score.js
@@ -1,2 +1,15 @@
 let score = document.getElementById("score");
+let newParagraph = document.createElement('p');
 
+let points = 0;
+
+// Function to check if the snake has eaten the food
+function eatFood() 
+{
+    if (parseInt(snake.style.left) === parseInt(food.style.left) && parseInt(snake.style.top) === parseInt(food.style.top)) 
+    {
+        newParagraph.innerHTML = "Score: ";
+        points = points + 1;//It counts points
+        score.innerHTML = newParagraph.innerHTML + points;
+    }
+}
