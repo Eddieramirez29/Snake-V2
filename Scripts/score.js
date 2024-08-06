066fa0cf8601fbeb4338ec00862a360e318bcddd
Eduardo <tkd13000@gmail.com>
2024-08-06 08:24:00 -0600

    Audio for eatenFood was added

--- Scripts/score.js
+++ Scripts/score.js
@@ -9,6 +9,7 @@ function eatFood()
     if (parseInt(snake.style.left) === parseInt(food.style.left) && parseInt(snake.style.top) === parseInt(food.style.top)) 
     {
         newParagraph.innerHTML = "Score: ";
+        eatFoodAuido.play();
         points = points + 1;//It counts points
         score.innerHTML = newParagraph.innerHTML + points;
         positionArrayX = getRandomPositionX(minX, maxX);
